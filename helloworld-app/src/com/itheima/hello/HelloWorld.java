85465399479b87f10f76bd278fc93c30d3084670
xinyuliurainbow <lxy2019302180211@163.com>
2024-01-30 22:20:30 +0800

    conflict test commit

--- helloworld-app/src/com/itheima/hello/HelloWorld.java
+++ helloworld-app/src/com/itheima/hello/HelloWorld.java
@@ -4,6 +4,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("hello world2");
-        System.out.println("hello world3");
+        System.out.println("hello world4");
     }
 }
