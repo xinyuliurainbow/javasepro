ae2d5a48e0461eb1a3be3c8d7bff71910fc67128
xinyuliurainbow <lxy2019302180211@163.com>
2024-01-30 22:38:06 +0800

    push test

--- helloworld-app/src/com/itheima/hello/HelloWorld.java
+++ helloworld-app/src/com/itheima/hello/HelloWorld.java
@@ -6,5 +6,6 @@ public class HelloWorld {
         System.out.println("hello world2");
         System.out.println("hello master");
         System.out.println("hello hotfix");
+        System.out.println("push test");
     }
 }
