8eef260d987a70309fd5964d0d1654f80476d5e2
xinyuliurainbow <lxy2019302180211@163.com>
2024-01-30 22:25:30 +0800

    master conflict test

--- helloworld-app/src/com/itheima/hello/HelloWorld.java
+++ helloworld-app/src/com/itheima/hello/HelloWorld.java
@@ -4,6 +4,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("hello world2");
-        System.out.println("hello world4");
+        System.out.println("hello master");
     }
 }
